1fccc5dfb265c98518a505c2f9152df736a69e4f
神灯 <1295106869@qq.com>
2019-08-16 23:17:44 +0800

    docs:功能全部注释

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-//返回 button按钮，本组件的点击事件触发父组件的点击事件
+//返回 button按钮，button上的onclic事件会触发 父组件的ontouch事件
 function Square(props) {
     return (
         <button
@@ -16,17 +16,18 @@ function Square(props) {
 
 class Board extends React.Component {
 
-
-
+    //渲染squar组件 可以直接写在render里面，但是不好看
     renderSquare(i) {
         return <Square
+            //父组件传递一个数组过来，value为父组件squares[i]
             value={this.props.squares[i]}
+            //子组件点击后会触发onTouch方法，然后触发父组件的ontouch方法，并穿参数
             onTouch={() => { this.props.onTouch(i) }}
         />;
     }
 
+    //渲染棋盘
     render() {
-
         return (
             <div>
                 <div className="board-row">
@@ -49,8 +50,10 @@ class Board extends React.Component {
     }
 }
 
+//最终渲染
 class Game extends React.Component {
 
+    //hisotry为历史记录，XisNext是下一步是X还是O，stepNmumber用来存放历史记录
     constructor(props) {
         super(props);
         this.state = {
@@ -62,15 +65,29 @@ class Game extends React.Component {
         }
     }
 
+    //ontouch事件触发的方法
     handleClick(i) {
+
+        //如果不根据setNumber进行截取，点击回退以后，在点击棋盘会导致未来的步数会错误，并且会一直累加hisotry数组
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
+
+        //得到最后一次点击的数据
         const current = history[history.length - 1];
+
+        //因为数组是地址引用，所以要创建一个新的数组
         const squares = current.squares.slice();
+
+        //判断游戏是否结束 如果结束，无法点击棋盘，判断是否当前点击的格子有内容，如果有，无法点击该格子
         if (calculateWinner(squares) || squares[i]) {
             return;
         }
 
+        //判断本次点击的生成的是X还是O
         squares[i] = this.state.xIsNext ? "X" : "O";
+
+        //更新状态，squares为一个新的数组，并且内容跟新过conncat方式存放在hisotry中
+        //XisNext取反，保证下次点击button上的value是否为X
+        //stepNumber为数组的长度，保证保证步数跟数组长度一致
         this.setState({
             history: history.concat([{
                 squares: squares
@@ -80,19 +97,26 @@ class Game extends React.Component {
         })
     }
 
+    //跳转历史继续方法，更新stepNumber与xIsnext
     jumpTo(step) {
         this.setState({
             stepNumber: step,
             xIsNext: (step % 2) === 0,
         });
     }
+
     render() {
+
+        //因为只是做渲染并未修改内容，所以不使用slice方法
         const history = this.state.history;
+
+        //当前展示的内容为stepNumber对应的内容
         const current = history[this.state.stepNumber];
 
+        //map遍历数组，返回一个虚拟DOM列表
         const moves = history.map((setp, move) => {
             const desc = move ?
-                'go to move #' + move :
+                "go to game at # " + move :
                 'go to game start';
             return (
                 <li key={move}>
@@ -100,6 +124,7 @@ class Game extends React.Component {
                 </li>
             )
         })
+
         let status;
         const winner = calculateWinner(current.squares);
         if (winner) {
@@ -149,4 +174,8 @@ function calculateWinner(squares) {
         }
     }
     return null;
+}
+
+function showRowAndCol(squares, index) {
+
 }
